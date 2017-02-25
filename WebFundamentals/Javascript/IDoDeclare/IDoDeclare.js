98b2e9485094deae76245d89f356c71fa73d4f69
Jacob Barnes <jacob.barnes8@gmail.com>
2017-02-25 10:16:42 -0500

    deleted file, began work on IDoDeclare

---
+++ WebFundamentals/Javascript/IDoDeclare/IDoDeclare.js
@@ -0,0 +1,29 @@
+var num1 = 10;
+console.log("The First Number", num1);
+
+var num2 = -8;
+console.log("The Second Number", num2);
+
+var num3 = 63.5;
+console.log("The Third Number", num3);
+
+var num4 = 102;
+console.log("The Fourth Number", num4);
+
+var string1 = bleh;
+console.log("The First String", string1);
+
+var string2 = blah;
+console.log("The Second String", string2);
+
+var string3 = blerg;
+console.log("The Third String", string3);
+
+var t = T;
+console.log("The True Boolean", t);
+
+var f = F;
+console.log("The False Boolean", f);
+
+var u = undefined;
+console.log("The Undefined Variable", u);
