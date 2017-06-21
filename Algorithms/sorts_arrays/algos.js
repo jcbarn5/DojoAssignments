95c257fbb4caa576633be304583eefe01e07f48a
Jacob Barnes <jacob.barnes8@gmail.com>
2017-06-21 18:04:33 -0400

    portfolio changes

--- Algorithms/sorts_arrays/algos.js
+++ Algorithms/sorts_arrays/algos.js
@@ -101,8 +101,48 @@ function CombineSorted(arr1,arr2) {
 //   return partIdx;
 // }
 
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
 
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)],
+        i       = left,
+        j       = right;
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
 
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        left = typeof left != "number" ? 0 : left;
+        right = typeof right != "number" ? items.length - 1 : right;
+        index = partition(items, left, right);
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(items, index, right);
+        }
+    }
+    console.log(items);
+    return items;
+}
 
 
 var my_arr = [5,4,9,2,5,3];
@@ -115,7 +155,7 @@ var my_sorted_arr1 = [-2,1,3,3,3,3,3];
 // InsertionSort(my_arr);
 // CombineSorted(my_sorted_arr1,my_sorted_arr2);
 // Partition(my_arr);
-
+quickSort(my_arr);
 
 
 
