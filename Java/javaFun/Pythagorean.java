6ce7d3486bbf62731012929549dc5fa1b7061e98
Jacob Barnes <jacob.barnes8@gmail.com>
2017-07-03 17:34:43 -0400

    started work on Java Beta

---
+++ Java/javaFun/Pythagorean.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Pythagorean {
+  public static void main (String[] args){
+    double legA, legB, legC;
+    Scanner in = new Scanner(System.in);
+    System.out.print("What value is side a: ");
+    legA = in.nextDouble();
+    System.out.print("What value is side b: ");
+    legB = in.nextDouble();
+
+    legC = Math.sqrt((legA*legA)+(legB*legB));
+    System.out.println("Hypotenuse: " + legC);
+    }
+}
---
+++ Java/javaFun/calculateHypotenuse.java
@@ -0,0 +1,6 @@
+// import java.lang.Math;
+// class calculateHypotenues {
+//   public static void main(String[] args) {
+//
+//   }
+// }
